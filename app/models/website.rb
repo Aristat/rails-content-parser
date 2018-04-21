36c9666a5855e6257c5b085b3ff93868009e2a5c
Alexandr Strelnikov <aristat37@gmail.com>
2018-04-21 23:37:16 +0300

    Fixed space

--- app/models/website.rb
+++ app/models/website.rb
@@ -4,5 +4,5 @@ class Website < ApplicationRecord
   has_many :website_contents, dependent: :destroy
 
   validates :url, presence: true,
-                  format: /\A(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\z/ix
+    format: /\A(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\z/ix
 end
